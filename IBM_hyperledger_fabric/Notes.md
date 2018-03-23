66d420c858806ce62d1e9812aabb7ed4f5f80f09
jaemnkm <jaemnkm@gmail.com>
2018-03-23 14:39:22 -0700

    IBM_hyperledger_fabric

---
+++ IBM_hyperledger_fabric/Notes.md
@@ -0,0 +1,4 @@
+### IBM's Hyperledger Fabric
+Learning their blockchain platform
+
+    docker run --name composer-playground --publish 8080:8080 hyperledger/composer-playground
---
+++ IBM_hyperledger_fabric/hello_world/lib/script.js
@@ -0,0 +1,11 @@
+/**
+ * New script file
+ */
+
+/**
+@param {org.acme.myfirstmodel.Hello} hello
+@transaction
+*/
+function hello(hello) {
+    console.log("Message: " + hello.test.message);
+  }
